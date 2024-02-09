3673e603ab5ad11186ac59f47d5bd66834b2fe4e
Antonio Tian <94172873+Kiwimmp@users.noreply.github.com>
2024-02-09 16:40:51 +0100

    update json

--- Pràctica 2/pokeapi.py
+++ Pràctica 2/pokeapi.py
@@ -1,21 +1,42 @@
 import requests
-import matplotlib.pyplot as plt
-from PIL import Image
-import numpy as np
+import json
 
-pokemon = input("Nombre de pokemon: ")
+def get_first_generation_pokemon():
+    url = "https://pokeapi.co/api/v2/pokemon?limit=151"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data['results']
+    else:
+        print("Error al obtener los datos de los Pokémon de la primera generación")
+        return []
 
-url = "https://pokeapi.co/api/v2/pokemon/" + pokemon
-res = requests.get(url)
+def get_pokemon_info(pokemon_url):
+    response = requests.get(pokemon_url)
+    if response.status_code == 200:
+        data = response.json()
+        pokemon_data = {
+            'name': data['name'].capitalize(),
+            'abilities': [ability['ability']['name'].capitalize() for ability in data['abilities']],
+            'type': data['types'][0]['type']['name'].capitalize(),
+            'stats': {stat['stat']['name'].capitalize(): stat['base_stat'] for stat in data['stats']},
+            'moves': [move['move']['name'].capitalize() for move in data['moves']]
+        }
+        return pokemon_data
+    else:
+        print(f"Error al obtener la información del Pokémon: {pokemon_url}")
+        return {}
 
-if res.status_code != 200:
-    print("No se ha encontrado el Pokemon")
-    exit()
+def main():
+    pokemon_first_generation = get_first_generation_pokemon()
+    pokemon_data = []
+    for pokemon in pokemon_first_generation:
+        pokemon_info = get_pokemon_info(pokemon['url'])
+        if pokemon_info:
+            pokemon_data.append(pokemon_info)
+    
+    with open('pokemon_first_generation.json', 'w') as file:
+        json.dump(pokemon_data, file, indent=4)
 
-# Abrir la imagen con Pillow y convertirla a un array numpy
-imagen_pillow = Image.open(requests.get(res.json()['sprites']['front_default'], stream=True).raw)
-imagen_np = np.array(imagen_pillow)
-
-plt.title(res.json()['name'])
-imgplot = plt.imshow(imagen_np)
-plt.show()
+if __name__ == "__main__":
+    main()
